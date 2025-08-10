2e8a976c67b6deaa079c649de43f01d6721aee08
Elshod <elshodibadullayev28@gmail.com>
2025-08-10 22:15:41 +0100

    INFRA:Initialize API Project

---
+++ Sheenam.Api/Program.cs
@@ -0,0 +1,22 @@
+//===================================================
+// Copyright (c) Coalition  of Good-Hearted Engineers
+// Free To use  Comfort and Peace
+//===================================================
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace Sheenam.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)=>
+          CreateHostBuilder(args).Build().Run();
+        
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                webBuilder.UseStartup<Startup>());
+    }
+}
