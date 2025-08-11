37a2e5e8a19516cba958650ebc0921eca24114ab
Elshod <elshodibadullayev28@gmail.com>
2025-08-11 15:11:42 +0100

    BROKERS: Insert Guests

--- Sheenam.Api/Brokers/Storages/StorageBroker.Guests.cs
+++ Sheenam.Api/Brokers/Storages/StorageBroker.Guests.cs
@@ -5,11 +5,21 @@
 
 using Microsoft.EntityFrameworkCore;
 using Sheenam.Api.Models.Foundations.Guests;
+using System.Threading.Tasks;
 
 namespace Sheenam.Api.Brokers.Storages
 {
     public partial class StorageBroker
     {
         public DbSet<Guest> Guests { get; set; }
+       public async ValueTask<Guest> InserGuestAsync(Guest guest)
+       {
+            using var broker=new StorageBroker(this.configuration);
+
+            var guestEntityEntry = await broker.Guests.AddAsync(guest);
+            await broker.SaveChangesAsync();
+
+            return guestEntityEntry.Entity;
+       }
     }
 }
