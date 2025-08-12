cab59bd601c9e8bc7d27e13aa5fa3c7455e95b63
Elshod <elshodibadullayev28@gmail.com>
2025-08-12 17:05:25 +0100

    ShouldThrowValidationExceptionOnAddIfGuestIsNullAndLogItAsync  -> WIP

--- Sheenam.Api.Tests.unit/Services/Foundations/Guests/GuestServiceTests.Validations.Add.cs
+++ Sheenam.Api.Tests.unit/Services/Foundations/Guests/GuestServiceTests.Validations.Add.cs
@@ -1,4 +1,9 @@
-﻿using Sheenam.Api.Models.Foundations.Guests;
+﻿//===================================================
+// Copyright (c) Coalition  of Good-Hearted Engineers
+// Free To Use  To Find Comfort and Peace
+//===================================================
+
+using Sheenam.Api.Models.Foundations.Guests;
 using Sheenam.Api.Models.Foundations.Guests.Exceptions;
 
 namespace Sheenam.Api.Tests.unit.Services.Foundations.Guests
