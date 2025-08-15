9dccacca9f5cb1002007c63803e8e9b94d192df2
Elshod <elshodibadullayev28@gmail.com>
2025-08-15 20:54:35 +0100

    ShouldThrowValidationExceptionOnAddIfGuestIsNullAndLogItAsync ->FAIL

--- Sheenam.Api.Tests.unit/Services/Foundations/Guests/GuestServiceTests.cs
+++ Sheenam.Api.Tests.unit/Services/Foundations/Guests/GuestServiceTests.cs
@@ -4,24 +4,30 @@
 //===================================================
 
 using Moq;
+using Sheenam.Api.Brokers.Loggings;
 using Sheenam.Api.Brokers.Storages;
 using Sheenam.Api.Models.Foundations.Guests;
 using Sheenam.Api.Services.Foundations.Guests;
+using System.Linq.Expressions;
 using Tynamix.ObjectFiller;
+using Xeptions;
 
 namespace Sheenam.Api.Tests.unit.Services.Foundations.Guests
 {
     public partial class GuestServiceTests
     {
         private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
         private readonly IGuestService guestService;
 
         public GuestServiceTests()
         {
             this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
 
-            this.guestService =
-                new GuestService(storageBroker: this.storageBrokerMock.Object);
+            this.guestService = new GuestService(
+                storageBroker: this.storageBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
         }
 
         private static Guest CreateRandomGuest() =>
@@ -29,6 +35,13 @@ namespace Sheenam.Api.Tests.unit.Services.Foundations.Guests
 
         private static DateTimeOffset GetRandomDateTimeOffset() =>
             new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private Expression<Func<Xeption, bool>> SameExceptionAs(Xeption expectedException)
+        {
+            return actualException => actualException.Message == actualException.Message
+            && actualException.InnerException.Message == expectedException.InnerException.Message
+            && (actualException.InnerException as Xeption).DataEquals(expectedException.InnerException.Data);
+        }
 
         private static Filler<Guest> CreateGuestFiller(DateTimeOffset date)
         {
--- Sheenam.Api/Services/Foundations/Guests/GuestService.cs
+++ Sheenam.Api/Services/Foundations/Guests/GuestService.cs
@@ -3,10 +3,10 @@
 // Free To Use  To Find Comfort and Peace
 //===================================================
 
+using Sheenam.Api.Brokers.Loggings;
 using Sheenam.Api.Brokers.Storages;
 using Sheenam.Api.Models.Foundations.Guests;
 using Sheenam.Api.Models.Foundations.Guests.Exceptions;
-using System;
 using System.Threading.Tasks;
 
 namespace Sheenam.Api.Services.Foundations.Guests
@@ -14,9 +14,15 @@ namespace Sheenam.Api.Services.Foundations.Guests
     public class GuestService : IGuestService
     {
         private readonly IStorageBroker storageBroker;
+        private readonly ILoggingBroker loggingBroker;
 
-        public GuestService(IStorageBroker storageBroker)=>
-        this.storageBroker = storageBroker;
+        public GuestService(
+            IStorageBroker storageBroker,
+            ILoggingBroker loggingBroker)
+        {
+            this.storageBroker = storageBroker;
+            this.loggingBroker = loggingBroker;
+        }
 
         public async ValueTask<Guest> AddGuestAsync(Guest guest)
         {
@@ -26,9 +32,7 @@ namespace Sheenam.Api.Services.Foundations.Guests
 
                 throw new GuestValidationException(nullGuestException);
             }
-
             return await this.storageBroker.InserGuestAsync(guest);
         }
-
     }
 }
