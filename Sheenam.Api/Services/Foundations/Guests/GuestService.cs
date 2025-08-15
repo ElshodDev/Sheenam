9f6fbfa189aa6e7f9b0cdcdc37cde935467e41fb
Elshod <elshodibadullayev28@gmail.com>
2025-08-15 21:18:34 +0100

    ShouldThrowValidationExceptionOnAddIfGuestIsNullAndLogItAsync -> PASS

--- Sheenam.Api/Services/Foundations/Guests/GuestService.cs
+++ Sheenam.Api/Services/Foundations/Guests/GuestService.cs
@@ -26,13 +26,24 @@ namespace Sheenam.Api.Services.Foundations.Guests
 
         public async ValueTask<Guest> AddGuestAsync(Guest guest)
         {
-            if (guest is null)
+            try
             {
-                var nullGuestException = new NullGuestException();
-
-                throw new GuestValidationException(nullGuestException);
+                if (guest is null)
+                {
+                    throw new NullGuestException();
+                }
+                return await this.storageBroker.InserGuestAsync(guest);
             }
-            return await this.storageBroker.InserGuestAsync(guest);
+            catch (NullGuestException nullGuestException) 
+            {
+                var guestValidationException =
+                    new GuestValidationException(nullGuestException);
+
+                this.loggingBroker.LogError(guestValidationException);
+
+
+                throw guestValidationException;
+            }
         }
     }
 }
