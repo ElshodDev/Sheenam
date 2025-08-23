6d76b6e537072416a03d212cbc9c88f6bc613ce5
Elshod <elshodibadullayev28@gmail.com>
2025-08-23 16:27:36 +0100

    ShouldThrowValidationExceptionOnAddIfHomeIsNullAndLogItAsync() -> WIP

---
+++ Sheenam.Api.Tests.unit/Services/Foundations/Homes/HomeServiceTests.Validations.Add.cs
@@ -0,0 +1,33 @@
+﻿//===================================================
+// Copyright (c) Coalition  of Good-Hearted Engineers
+// Free To Use  To Find Comfort and Peace
+//===================================================
+
+using Sheenam.Api.Models.Foundations.Homes;
+using Sheenam.Api.Models.Foundations.Homes.Exceptions;
+
+namespace Sheenam.Api.Tests.unit.Services.Foundations.Homes
+{
+    public partial class HomeServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfHomeIsNullAndLogItAsync()
+        {
+            //given
+            Home nullHome = null;
+            var nullHomeException = new NullHomeException();
+
+            var expectedHomeValidationException =
+                new HomeValidationException(nullHomeException);
+
+            //when
+            ValueTask<Home> addHomeTask =
+                 this.homeService.AddHomeAsync(nullHome);
+
+            //then
+            await Assert.ThrowsAsync<HomeValidationException>(() =>
+            addHomeTask.AsTask());
+
+        }
+    }
+}
