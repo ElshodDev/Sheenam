840f78fa358188373ae1812278f4bcfe4578631e
Elshod <elshodibadullayev28@gmail.com>
2025-11-03 01:34:55 +0500

    DATA: Model & Migrate  Hosts

---
+++ Sheenam.Api/Models/Foundations/Hosts/Host.cs
@@ -0,0 +1,21 @@
+﻿//===================================================
+// Copyright (c) Coalition  of Good-Hearted Engineers
+// Free To Use  To Find Comfort and Peace
+//===================================================
+
+using Sheenam.Api.Models.Foundations.Guests;
+using System;
+
+namespace Sheenam.Api.Models.Foundations.Hosts
+{
+    public class Host
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTimeOffset DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public  GenderType Gender { get; set; }
+    }
+}
