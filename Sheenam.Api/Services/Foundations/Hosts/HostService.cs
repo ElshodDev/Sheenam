3b8e6015c15643f448a3d516c433dbf1d775fc3f
Elshod <elshodibadullayev28@gmail.com>
2025-11-05 00:40:19 +0500

    ShouldAddHostAsync -> FAIL

---
+++ Sheenam.Api/Services/Foundations/Hosts/HostService.cs
@@ -0,0 +1,25 @@
+﻿//===================================================
+// Copyright (c) Coalition  of Good-Hearted Engineers
+// Free To Use  To Find Comfort and Peace
+//===================================================
+
+using Sheenam.Api.Brokers.Storages;
+using Sheenam.Api.Models.Foundations.Hosts;
+using System;
+using System.Threading.Tasks;
+
+namespace Sheenam.Api.Services.Foundations.Hosts
+{
+    public partial class HostService : IHostService
+    {
+        private readonly IStorageBroker storageBroker;
+
+        public HostService(IStorageBroker storageBroker) =>
+            this.storageBroker = storageBroker;
+
+
+        public ValueTask<Host> AddHostAsync(Host host) =>
+            throw new NotImplementedException();
+
+    }
+}
