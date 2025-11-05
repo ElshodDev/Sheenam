ec2b7678dea47f301f889225157ae4e021666f18
Elshod <elshodibadullayev28@gmail.com>
2025-11-05 08:55:48 +0500

    ShouldThrowValidationExceptionOnAddIfHostIsNullAndLogItAsync -> WIP

---
+++ Sheenam.Api.Tests.unit/Services/Foundations/Hosts/HostServiceTests.Validations.Add.cs
@@ -0,0 +1,37 @@
+﻿//===================================================
+// Copyright (c) Coalition  of Good-Hearted Engineers
+// Free To Use  To Find Comfort and Peace
+//===================================================
+
+using FluentAssertions;
+using Moq;
+using Sheenam.Api.Models.Foundations.Hosts;
+using Sheenam.Api.Models.Foundations.Hosts.Exceptions;
+
+namespace Sheenam.Api.Tests.Unit.Services.Foundations.Hosts
+{
+    public partial class HostServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfHostIsNullAndLogItAsync()
+        {
+            // given
+            Host nullHost = null;
+
+            var nullHostException =
+                new NullHostException();
+
+            var expectedHostValidationException =
+                new HostValidationException(nullHostException);
+
+            // when
+
+            ValueTask<Host> addHostTask =
+               this.hostService.AddHostAsync(nullHost);
+
+            // then
+            await Assert.ThrowsAsync<HostValidationException>(() =>
+               addHostTask.AsTask());
+        }
+    }
+}
