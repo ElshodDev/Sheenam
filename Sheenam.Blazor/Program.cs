d6928c7cf8d46397a8e65f9f7b6137ed25df0260
Elshod <elshodibadullayev28@gmail.com>
2025-11-15 13:20:58 +0500

    INFRA: Initialize  Frontend Project

---
+++ Sheenam.Blazor/Program.cs
@@ -0,0 +1,37 @@
+//===================================================
+// Copyright (c) Coalition  of Good-Hearted Engineers
+// Free To Use  To Find Comfort and Peace
+//===================================================
+
+using Sheenam.Blazor.Components;
+
+namespace Sheenam.Blazor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            var app = builder.Build();
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAntiforgery();
+            app.MapStaticAssets();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
