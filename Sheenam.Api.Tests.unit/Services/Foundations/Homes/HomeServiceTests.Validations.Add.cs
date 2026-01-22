22acf8172d3b79814699a62caac30d238d4e0488
Elshod Ibodullayev <elshod@example.com>
2026-01-22 16:21:30 +0500

    INFRA: Homes and Tests Improve

--- Sheenam.Api.Tests.unit/Services/Foundations/Homes/HomeServiceTests.Validations.Add.cs
+++ Sheenam.Api.Tests.unit/Services/Foundations/Homes/HomeServiceTests.Validations.Add.cs
@@ -3,6 +3,7 @@
 // Free To Use To Find Comfort and Peace
 //===================================================
 
+using FluentAssertions;
 using Moq;
 using Sheenam.Api.Models.Foundations.Homes;
 using Sheenam.Api.Models.Foundations.Homes.Exceptions;
@@ -53,7 +54,7 @@ namespace Sheenam.Api.Tests.unit.Services.Foundations.Homes
         [InlineData("")]
         [InlineData(" ")]
         public async Task ShouldThrowValidationExceptionOnAddIfHomeIsInvalidAndLogItAsync(
-            string invalidText)
+     string invalidText)
         {
             // given
             var invalidHome = new Home
@@ -62,30 +63,53 @@ namespace Sheenam.Api.Tests.unit.Services.Foundations.Homes
                 HostId = Guid.Empty,
                 Address = invalidText,
                 AdditionalInfo = invalidText,
-                Type = (HouseType)999
+                Area = 0,               // Xato (<= 0)
+                Type = (HouseType)999,  // Xato (not defined)
+                ListingType = (ListingType)999, // Xato (not defined)
+                ListedDate = default    // Xato (default)
             };
 
             var invalidHomeException = new InvalidHomeException();
 
+            // 1
             invalidHomeException.UpsertDataList(
                 key: nameof(Home.Id),
                 value: "Id is required");
 
+            // 2
             invalidHomeException.UpsertDataList(
                 key: nameof(Home.HostId),
                 value: "Id is required");
 
+            // 3
             invalidHomeException.UpsertDataList(
                 key: nameof(Home.Address),
                 value: "Text is required");
 
+            // 4
             invalidHomeException.UpsertDataList(
                 key: nameof(Home.AdditionalInfo),
                 value: "Text is required");
 
+            // 5
+            invalidHomeException.UpsertDataList(
+                key: nameof(Home.Area),
+                value: "Number must be greater than zero");
+
+            // 6
             invalidHomeException.UpsertDataList(
                 key: nameof(Home.Type),
                 value: "Value is invalid");
+
+            // 7
+            invalidHomeException.UpsertDataList(
+                key: nameof(Home.ListingType),
+                value: "Value is invalid");
+
+            // 8
+            invalidHomeException.UpsertDataList(
+                key: nameof(Home.ListedDate),
+                value: "Date is required");
 
             var expectedHomeValidationException =
                 new HomeValidationException(invalidHomeException);
@@ -93,6 +117,25 @@ namespace Sheenam.Api.Tests.unit.Services.Foundations.Homes
             // when
             ValueTask<Home> addHomeTask =
                 this.homeService.AddHomeAsync(invalidHome);
+
+            HomeValidationException actualHomeValidationException =
+                await Assert.ThrowsAsync<HomeValidationException>(
+                    addHomeTask.AsTask);
+
+            // then
+            actualHomeValidationException.Should().BeEquivalentTo(expectedHomeValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedHomeValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertHomeAsync(It.IsAny<Home>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
         }
     }
 }
