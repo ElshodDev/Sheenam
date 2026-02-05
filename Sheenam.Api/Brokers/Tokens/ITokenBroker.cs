532901bf6ec2302fc85f8fcab0b8ce65a9b69985
Elshod Ibodullayev <elshod@example.com>
2026-02-06 00:13:49 +0500

    FEAT: Role Access Control

---
+++ Sheenam.Api/Brokers/Tokens/ITokenBroker.cs
@@ -0,0 +1,14 @@
+﻿//===================================================
+// Copyright (c) Coalition  of Good-Hearted Engineers
+// Free To Use  To Find Comfort and Peace
+//===================================================
+
+using Sheenam.Api.Models.Foundations.Users;
+
+namespace Sheenam.Api.Brokers.Tokens
+{
+    public interface ITokenBroker
+    {
+        string GenerateToken(User user);
+    }
+}
---
+++ Sheenam.Api/Brokers/Tokens/TokenBroker.cs
@@ -0,0 +1,45 @@
+﻿//===================================================
+// Copyright (c) Coalition  of Good-Hearted Engineers
+// Free To Use  To Find Comfort and Peace
+//===================================================
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Sheenam.Api.Models.Foundations.Users;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Sheenam.Api.Brokers.Tokens
+{
+    public class TokenBroker : ITokenBroker
+    {
+        private readonly IConfiguration configuration;
+
+        public TokenBroker(IConfiguration configuration) =>
+            this.configuration = configuration;
+
+        public string GenerateToken(User user)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Role, user.Role.ToString()) // ROLNI SHU YERDA QO'SHAMIZ
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: configuration["Jwt:Issuer"],
+                audience: configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
