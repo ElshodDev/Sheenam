ca53628b956666c87a4c264d519e09779d96efb0
Elshod Ibodullayev <elshod@example.com>
2026-02-15 01:39:31 +0500

    Views: Add GuestComponent and GuestsPage to display all guests

---
+++ Sheenam.Blazor/Views/Components/GuestComponent.razor.cs
@@ -0,0 +1,24 @@
+﻿//===================================================
+// Copyright (c) Coalition  of Good-Hearted Engineers
+// Free To Use  To Find Comfort and Peace
+//===================================================
+
+using Microsoft.AspNetCore.Components;
+using Sheenam.Blazor.Models.Foundations.Guests;
+using Sheenam.Blazor.Services.Foundations.Guests;
+
+namespace Sheenam.Blazor.Views.Components
+{
+    public partial class GuestComponent : ComponentBase
+    {
+        [Inject]
+        public IGuestService GuestService { get; set; }
+
+        public IEnumerable<Guest> Guests { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            this.Guests = await this.GuestService.RetrieveAllGuestsAsync();
+        }
+    }
+}
