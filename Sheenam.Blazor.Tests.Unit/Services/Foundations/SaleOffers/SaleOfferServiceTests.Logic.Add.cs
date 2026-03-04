06f6f4c5665abdd46624a4fc4c57c4681d7e35af
Elshod Ibodullayev <elshod@example.com>
2026-03-04 16:54:42 +0500

    test: add SaleOffers unit tests (Logic, Validations, Exceptions)

---
+++ Sheenam.Blazor.Tests.Unit/Services/Foundations/SaleOffers/SaleOfferServiceTests.Logic.Add.cs
@@ -0,0 +1,41 @@
+﻿//===================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//===================================================
+using FluentAssertions;
+using Moq;
+using Sheenam.Blazor.Models.Foundations.SaleOffers;
+
+namespace Sheenam.Blazor.Tests.Unit.Services.Foundations.SaleOffers
+{
+    public partial class SaleOfferServiceTests
+    {
+        [Fact]
+        public async Task ShouldAddSaleOfferAsync()
+        {
+            // given
+            SaleOffer randomSaleOffer = CreateRandomSaleOffer();
+            SaleOffer inputSaleOffer = randomSaleOffer;
+            SaleOffer retrievedSaleOffer = inputSaleOffer;
+            SaleOffer expectedSaleOffer = retrievedSaleOffer;
+
+            this.apiBrokerMock.Setup(broker =>
+                broker.PostSaleOfferAsync(inputSaleOffer))
+                    .ReturnsAsync(retrievedSaleOffer);
+
+            // when
+            SaleOffer actualSaleOffer =
+                await this.saleOfferService.AddSaleOfferAsync(inputSaleOffer);
+
+            // then
+            actualSaleOffer.Should().BeEquivalentTo(expectedSaleOffer);
+
+            this.apiBrokerMock.Verify(broker =>
+                broker.PostSaleOfferAsync(inputSaleOffer),
+                    Times.Once);
+
+            this.apiBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ Sheenam.Blazor.Tests.Unit/Services/Foundations/SaleOffers/SaleOfferServiceTests.cs
@@ -0,0 +1,58 @@
+﻿//===================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//===================================================
+using Moq;
+using Sheenam.Blazor.Brokers.Apis;
+using Sheenam.Blazor.Brokers.Loggings;
+using Sheenam.Blazor.Models.Foundations.SaleOffers;
+using Sheenam.Blazor.Services.Foundations.SaleOffers;
+using System.Linq.Expressions;
+using Tynamix.ObjectFiller;
+using Xeptions;
+
+namespace Sheenam.Blazor.Tests.Unit.Services.Foundations.SaleOffers
+{
+    public partial class SaleOfferServiceTests
+    {
+        private readonly Mock<IApiBroker> apiBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly ISaleOfferService saleOfferService;
+
+        public SaleOfferServiceTests()
+        {
+            this.apiBrokerMock = new Mock<IApiBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+            this.saleOfferService = new SaleOfferService(
+                apiBroker: this.apiBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private static SaleOffer CreateRandomSaleOffer() =>
+            CreateSaleOfferFiller().Create();
+
+        private static Filler<SaleOffer> CreateSaleOfferFiller()
+        {
+            var filler = new Filler<SaleOffer>();
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(DateTimeOffset.UtcNow)
+                .OnType<DateTimeOffset?>().Use(() => (DateTimeOffset?)null)
+                .OnType<decimal>().Use(() => new Random().Next(1, 100000));
+            return filler;
+        }
+
+        private Expression<Func<Exception, bool>> SameExceptionAs(Exception expectedException)
+        {
+            return actualException =>
+                actualException.Message == expectedException.Message
+                && actualException.InnerException.Message == expectedException.InnerException.Message
+                && (actualException.InnerException as Xeption).DataEquals(expectedException.InnerException.Data);
+        }
+
+        private static IQueryable<SaleOffer> CreateRandomSaleOffers() =>
+            CreateSaleOfferFiller().Create(count: GetRandomNumber()).AsQueryable();
+
+        private static int GetRandomNumber() =>
+            new IntRange(min: 2, max: 10).GetValue();
+    }
+}
